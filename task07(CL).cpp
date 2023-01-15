dea64d790245104aaaccae7f8c7bf9bcb38319a9
NomanAli42856 <121042827+NomanAli42856@users.noreply.github.com>
2023-01-15 15:49:11 +0500

    Add files via upload

---
+++ task07(CL).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+main ()
+{
+    system("cls");
+    int num;
+    cout << "Enter the cost of the dress : ";
+    cin >> cost;
+    if (num > 50)
+    {
+        cout << "You have passed.";
+    }
+    else
+    {
+        cout << "You have failed.";
+    }
+    return 0;
+}

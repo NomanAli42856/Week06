aed4fcdbcdaf1c3ab6e0de22362552bbce9e7d5e
NomanAli42856 <121042827+NomanAli42856@users.noreply.github.com>
2023-01-15 15:39:21 +0500

    Add files via upload

---
+++ task03(OP).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+main ()
+{
+    system("cls");
+    int rollno;
+    string name;
+    cout << "Enter your name : ";
+    cin >> cost;
+    cout << "Enter your Roll No : ";
+    cin >> rollno;
+    if (rollno == 501 && name == "ali")
+    {
+        cout << "Congratulations!";
+    }
+
+}
---
+++ task04(CL).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+main ()
+{
+    system("cls");
+    int cost;
+    string brand;
+    cout << "Enter the cost of the dress : ";
+    cin >> cost;
+    cout << "Enter brand of the dress : ";
+    cin >> brand;
+    if (cost < 1500)
+    {
+        if(brand == "MTJ")
+        {
+           cout << "Buy the dress";
+        }
+    }
+
+}
